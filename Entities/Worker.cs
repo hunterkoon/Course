2437307a564161f64c2c8343db20564d2eab5e72
GABRIEL CAMPOS <g.p.campos@uni9.edu.br>
2021-10-17 00:16:37 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Worker.cs
@@ -0,0 +1,14 @@
+﻿
+using Course.Entities.Enums;
+
+namespace Course.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+
+    }
+}
