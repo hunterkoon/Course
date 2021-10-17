5a4ebf30b61cb62077430ec058e5221e423fa650
GABRIEL CAMPOS <g.p.campos@uni9.edu.br>
2021-10-17 14:30:27 -0300

    Curso C# UDEMY

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Course.Entities;
 using Course.Entities.Enums;
 using System;
+using System.Globalization;
 
 namespace Course
 {
@@ -8,7 +9,52 @@ namespace Course
     {
         static void Main(string[] args)
         {
-          
+
+            Console.Write("Enter department's name: ");
+            
+            string deptName = Console.ReadLine();
+            Console.WriteLine("Enter contract data: ");
+            
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine( "Level(Junior,MidLevel/Senior): " );
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            
+            Console.Write("Base salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
+            Console.Write("How many contracts to this worker? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1;  i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} contract data:: ");
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duration (hours): ");
+                int hours = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+                worker.AddContract(contract);
+
+            }
+
+                Console.WriteLine();
+                Console.Write("Enter with mouth and year to calculate income (MM/YYYY): ");
+                string mounthAndYear = Console.ReadLine();
+                int mouth = int.Parse(mounthAndYear.Substring(0, 2));
+                int year = int.Parse(mounthAndYear.Substring(3));
+
+                Console.WriteLine("Name " + worker.Name);
+                Console.WriteLine("Department " + worker.Department.Name);
+                Console.WriteLine("Income for " + mounthAndYear + " : " + worker.Income(year, mouth).ToString("F2",CultureInfo.InvariantCulture));
+
+
         }
     }
 }
